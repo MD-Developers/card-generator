949fd37cbeb03c7fc5d2fa420257b3cecb449ebd
Diego Perozo <perozodiego@gmail.com>
2021-04-16 17:38:01 -0400

    Init

---
+++ README.md
@@ -0,0 +1,37 @@
+<!-- <h1 align="center">
+  <a href="https://telebook.netlify.app" aria-hidden="true"><img src="https://raw.githubusercontent.com/DiegoPerozo/tele-book/master/img/thumbail%20.jpg" alt="Tele-BOOK" width="1200" /></a>
+</h1> -->
+
+Un generador de imagenes con texto basado en canvas en el cual se le puede agregar cualquier texto en 3 distintas posiciones, cambir de imagen y descargar la imagen generada.
+
+<h2><a id="tabla-de-contenido" href="#tabla-de-contenido" aria-hidden="true">#</a> Tabla de contenidos</h2>
+
+- [Caracteristicas](#features)
+- [Herramientas](#tools)
+- [Creditos](#credits)
+- [Licencia](#licencia)
+
+<h2><a id="features" href="#features" aria-hidden="true">#</a> Caracteristicas</h2>
+
+- Se puede seleccionar cualquier imagne para su uso.
+- No se guarda ningun dato de la imagne generada o los textos utilizados.
+- Puede escribir en 3 posiciones distintas de la imagen y a su vez mover el texto central simplemente dandole click y arrastrando el mouse.
+- Puede cambiar individualmente el color de cada texto asi como el tipo de fuente entre 4 para elegir.
+- Puede cambiar el fondo base del canvas.
+- Puede cambiar la opacidad de la imagen agregada.
+- Puede cambiar el tamaño proporcional de la imagen agregada.
+- Puede descargar la imagen en formato '.png'.
+
+<h2><a id="tools" href="#tools" aria-hidden="true">#</a> Herramientas</h2>
+
+- Visual Studio Code con Live Server
+- Netlify
+
+<h2><a id="credits" href="#credits" aria-hidden="true">#</a> Creditos</h2>
+
+**Creado y mantenido por [Milagros Pirona](https://github.com/MilagrosPirona) & [Diego Perozo](https://github.com/DiegoPerozo).**
+
+<h2><a id="licencia" href="#licencia" aria-hidden="true">#</a> Licencia</h2>
+
+Copyright &copy; 2020 Diego Perozo & Milagros Pirona. 
+Licensed under **[the MIT license](LICENSE.md)**.
---
+++ js/main.js
@@ -0,0 +1,296 @@
+const img = document.getElementById('start-img');
+const imageFile = document.getElementById('imageFile');
+
+const titulo = document.getElementById("titulo");
+titulo.value = "";
+
+const mensaje = document.getElementById("mensaje");
+mensaje.value = "";
+
+const nombres = document.getElementById("nombres");
+nombres.value = "";
+const colorNombre = document.getElementById("colorNombre");
+
+let nombresObj = {
+    texto: '',
+    x: 0,
+    y: 0,
+}
+var selectedText = -1;
+var offsetX = 0;
+var offsetX = 0;
+
+const opacidad = document.getElementById("opacidad");
+
+const color = document.getElementById("color");
+const colorTitulo = document.getElementById("colorTitulo");
+const colorMensaje = document.getElementById("colorMensaje");
+const escala = document.getElementById("escala");
+
+const titleFont = document.getElementById("titleFont");
+var fuenteTitulo = "Courier";
+
+const msgFont = document.getElementById("msgFont");
+var fuenteMensaje = "Courier";
+
+const namesFont = document.getElementById("namesFont");
+var fuenteNombres = "Courier";
+
+const canvas = document.getElementById('birthday');
+const ctx = canvas.getContext("2d");
+
+const imagen = new Image();
+
+//Iniciar valores por defecto del canvas.
+initCanvas();
+
+//EventListeners para vigilar los click e inputs.
+imageFile.addEventListener("change", handleImage);
+
+titulo.addEventListener("keyup", render);
+mensaje.addEventListener("keyup", render);
+
+opacidad.addEventListener("input", render);
+
+color.addEventListener("change", render);
+colorTitulo.addEventListener("change", render);
+colorMensaje.addEventListener("change", render);
+escala.addEventListener("input", render);
+document.getElementById("descargar").addEventListener("click", descargar);
+
+titleFont.addEventListener('change', (event) => {
+    fuenteTitulo = event.target.value;
+    render();
+});
+msgFont.addEventListener('change', (event) => {
+    fuenteMensaje = event.target.value;
+    render();
+});
+namesFont.addEventListener('change', (event) => {
+    fuenteNombres = event.target.value;
+    render();
+});
+
+nombres.addEventListener("keyup", () => {  //Cambiamos :P
+    nombresObj.texto = nombres.value;
+    render();
+});
+colorNombre.addEventListener("change", render);
+
+//Creamos nuevos Listeners :D
+canvas.addEventListener('mousedown', handleMouseDown);
+canvas.addEventListener('mousemove', handleMouseMove);
+canvas.addEventListener('mouseup', handleMouseUp);
+canvas.addEventListener('mouseout', handleMouseOut);
+
+/*******************+/
+/******Fnciones******/
+/********************/
+function initCanvas() {
+    let deviceWidth = window.innerWidth;
+    canvasWidth = Math.min(450, deviceWidth - 20);
+    canvasHeight = Math.min(480, deviceWidth - 20);
+
+    canvas.width = canvasWidth;
+    canvas.height = canvasHeight;
+
+    //Iniciamos las posiciones de X y Y de nuestro objeto de Nombre.
+    nombresObj.x =  canvas.width/2;
+    nombresObj.y =  canvas.height/2;
+    offsetX = canvas.offsetLeft;
+    offsetY = canvas.offsetTop;
+
+    //Colocamos el fondo del color por defecto, que sera e gris.
+    drawBackground();
+
+    //Ejecutamos una funcion cuando la imagen esta cargada al 100%
+    img.onload = function() {
+        scaleToFill(this);
+        imagen.src = '../img/default.jpg';
+    }
+    img.onerror = function() {
+        console.log("Error al cargar la imagen por defecto.");
+    }
+}
+function drawBackground() {
+    ctx.beginPath();
+    ctx.rect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = color.value ? color.value:'#d6d6d6';
+    ctx.fill();
+}
+function scaleToFill(img){
+    //Obtenemos una ecala segun el ancho y alto del canvas y la imagen.
+    //Esta escala la usamos para hacer que la imagen cambie al tamaño del canvas.
+    let scale = Math.max(canvas.width / img.width, canvas.height / img.height) * escala.value;
+
+    //Usamos el tamaño del canvas y de la imagen para
+    //obtener la posicion 'X' y 'Y' para que quede centrada.
+    let x = (canvas.width / 2) - (img.width / 2) * scale;
+    let y = (canvas.height / 2) - (img.height / 2) * scale;
+    
+    // Dibujamos la imagne en el canvas.
+    ctx.drawImage(img, x, y, img.width * scale, img.height * scale);
+}
+async function handleImage(e) {
+    let file = e.target.files[0];
+    const result = await toBase64(file).catch(e => Error(e));
+    if(result instanceof Error) {
+        console.log('Error: ', result.message);
+        return;
+    }
+    imagen.src = result;
+    imagen.onload = () => {
+        render();
+    };
+}
+function toBase64(file){
+    return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = () => resolve(reader.result);
+        reader.onerror = error => reject(error);
+    });
+}
+function render() {
+    drawBackground();
+    drawImage();
+    drawText("top", (canvas.width/2)-8, 50);
+    drawText("bottom", canvas.width/2, canvas.height - 75);
+    drawText("middle", canvas.width/2, canvas.height/2);
+}
+function drawImage() {
+    ctx.save();
+    ctx.globalAlpha = opacidad.value;
+    scaleToFill(imagen);
+    ctx.globalAlpha = 1;
+    ctx.restore();
+}
+function drawText(text, positionX, positionY) {
+    ctx.fillStyle = "white";
+    ctx.font = "17pt Courier";
+    
+    ctx.lineWidth  = 5;
+    ctx.strokeStyle = 'black';
+    ctx.textAlign = 'center';
+    ctx.lineJoin = 'round';
+
+    // Cambiamos nuestra variable 'top' para que haga salto de linea automatico.
+    const top = saltosDeLinea(titulo.value).map((linea) => Object.assign({}, {
+        text: `${linea}`,
+        font: `bolder 17pt ${fuenteTitulo}`,
+        fillStyle: colorTitulo.value
+    }));
+
+    const bottom = saltosDeLinea(mensaje.value).map((linea, index) => Object.assign({}, {
+        text: `${linea}`,
+        font: `bolder 17pt ${fuenteMensaje}`,
+        fillStyle: colorMensaje.value
+    }));
+
+    //Variable "middle" para mostrar los nombres en el medio.
+    const middle = saltosDeLinea(nombresObj.texto).map((linea, index) => Object.assign({}, {
+        text: `${linea}`,
+        font: `bolder 17pt ${fuenteNombres}`,
+        fillStyle: colorNombre.value
+    }));
+
+    let position = [];
+    switch (text) {
+        case 'top':
+            position = top;
+            break;
+        case 'bottom':
+            position = bottom;
+            break;
+        case 'middle':
+            position = middle;
+            positionX = nombresObj.x;
+            positionY = nombresObj.y;
+            break;
+    }
+
+    fillMixedText(ctx, position, positionX, positionY);
+}
+function fillMixedText(ctx, args, x, y) {
+    let defaultFillStyle = ctx.fillStyle;
+    let defaultFont = ctx.font;
+
+    ctx.save();
+    args.forEach(({
+        text,
+        fillStyle,
+        font
+    }) => {
+        ctx.fillStyle = fillStyle || defaultFillStyle;
+        ctx.font = font || defaultFont;
+        
+
+        ctx.strokeText(text, x, y);
+        ctx.fillText(text, x, y);
+
+        y += ctx.measureText(text).actualBoundingBoxAscent + ctx.measureText(text).actualBoundingBoxDescent + 10;
+    });
+    ctx.restore();
+}
+function descargar() {
+    let nombre = new Date().toString();
+
+    let link = document.createElement("a");
+    link.download = `${nombre}.png`;
+    link.href = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
+    link.click();
+}
+
+function saltosDeLinea(str){ //nuevo
+    return str.match(/[\s\S]{1,30}/g) || [];
+}
+
+//Mover Nombre
+function textHitBox(x, y) {
+  let texto = nombresObj;
+  return (x >= texto.x - 300 && x <= texto.x + 300 && y >= texto.y - 30 && y <= texto.y + 30);
+}
+
+function handleMouseDown(e) {
+    e.preventDefault();
+    startX = parseInt(e.clientX - offsetX);
+    startY = parseInt(e.clientY - offsetY);
+
+    if(textHitBox(startX, startY))
+        selectedText = 1;
+}
+function handleMouseUp(e) {
+    e.preventDefault();
+    selectedText = -1;
+}
+function handleMouseOut(e) {
+    e.preventDefault();
+    selectedText = -1;
+}
+function handleMouseMove(e) {
+    if (selectedText < 0) {
+        return;
+    }
+    e.preventDefault();
+    mouseX = parseInt(e.clientX - offsetX);
+    mouseY = parseInt(e.clientY - offsetY);
+
+    var dx = mouseX - startX;
+    var dy = mouseY - startY;
+    startX = mouseX;
+    startY = mouseY;
+
+    nombresObj.x += dx;
+    nombresObj.y += dy;
+    render();
+}
+
+//Extra
+function scaleToFit(img){
+    // get the scale
+    let scale = Math.min(canvas.width / img.width, canvas.height / img.height);
+    // get the top left position of the image
+    let x = (canvas.width / 2) - (img.width / 2) * scale;
+    let y = (canvas.height / 2) - (img.height / 2) * scale;
+    ctx.drawImage(img, x, y, img.width * scale, img.height * scale);
+}
